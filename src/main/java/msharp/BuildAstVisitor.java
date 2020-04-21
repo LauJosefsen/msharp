0438f32714a7c463a93ff07de20080986fc44c5a
LauJosefsen <ljosef18@student.aau.dk>
2020-04-21 14:05:46 +0200

    Added test cases for PbodyToneAST Visitor

--- src/main/java/msharp/BuildAstVisitor.java
+++ src/main/java/msharp/BuildAstVisitor.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-class BuildAstVisitor extends cfgBaseVisitor<Node> {
+public class BuildAstVisitor extends cfgBaseVisitor<Node> {
     List<String> getSemanticErrors() {
         return semanticErrors;
     }
--- src/test/java/BuildAstVisitorTests.java
+++ src/test/java/BuildAstVisitorTests.java
@@ -1,10 +1,59 @@
-import org.junit.jupiter.api.Test;
+import msharp.BuildAstVisitor;
+import msharp.Nodes.Node;
+import msharp.Nodes.NoteNode;
+import msharp.Nodes.ProgNode;
+import msharp.Nodes.StmtNode;
+import msharp.cfgLexer;
+import msharp.cfgParser;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BuildAstVisitorTests {
+    // Utility method for creating a parse tree from an input string
+    private ParseTree MakeParseTreeWithAntlrFromString(String inputStr){
+        CharStream input = CharStreams.fromString(inputStr);
+        cfgLexer lexer = new cfgLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        cfgParser parser = new cfgParser(tokens);
+        return parser.prog();
+    }
+    
+    
+    
+    @RepeatedTest(value=10, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+    @DisplayName("VisitPbodyToneWithDifferentOctaves")
+    void testVisitPbodyToneWithDifferentOctaves(RepetitionInfo repInfo, TestInfo testInfo){
+        // Arrange
+        BuildAstVisitor astVisitor = new BuildAstVisitor();
+        String inputStr = "play \n a" + repInfo.getCurrentRepetition() + "\n end play";
+
+        // Act
+        ProgNode ast = (ProgNode) astVisitor.visit(MakeParseTreeWithAntlrFromString(inputStr));
+        NoteNode note = (NoteNode) ast.main.stmts.get(0);
+
+        // Assert
+        assertEquals(note.getOctave(), repInfo.getCurrentRepetition());
+        assertEquals('a', note.getLetter());
+    }
+
+
     @Test
-    void justAnExample() {
-        assertTrue("2".contains("2"));
+    void testVisitPbodyToneWithoutOctave(){
+        // Arrange
+        BuildAstVisitor astVisitor = new BuildAstVisitor();
+        String inputStr = "play \n a \n end play";
+
+        // Act
+        ProgNode ast = (ProgNode) astVisitor.visit(MakeParseTreeWithAntlrFromString(inputStr));
+        NoteNode note = (NoteNode) ast.main.stmts.get(0);
+
+        // Assert
+        assertEquals(note.getOctave(), -1);
+        assertEquals('a', note.getLetter());
     }
 }
