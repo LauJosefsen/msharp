71e9b09d5fe487cf7e33ca4056745372d8f869ca
LarsNyborgPedersen <larsnyborgpedersen@gmail.com>
2020-05-19 10:57:32 +0200

    Made some tests for BuildNoteListVisitor

--- src/test/java/msharp/NotePopulation/BuildNoteListVisitorTests.java
+++ src/test/java/msharp/NotePopulation/BuildNoteListVisitorTests.java
@@ -30,11 +30,13 @@ class BuildNoteListVisitorTests {
     void setUp ()
     {
         List<NodeContext> nodeContexts = new ArrayList<NodeContext>();
+        initialCtx = new NodeContext();
+        ctxToSend = new NodeContext();
         nodeContexts.add(initialCtx);
         nodeContexts.add(ctxToSend);
         
         for (NodeContext nodeContext : nodeContexts) {
-            nodeContext= new NodeContext();
+            nodeContext = new NodeContext();
             nodeContext.bpm = new Bpm(150, new Fraction(1, 4));
             nodeContext.tempo = new Fraction(1, 8);
             nodeContext.instrument = Instrument.HARP;
@@ -44,107 +46,116 @@ class BuildNoteListVisitorTests {
         visitor = new BuildNoteListVisitor(symbolTable);
     }
     
-    void createSymbolTable() {
+    void createSymbolTable ()
+    {
         //Creating global variables: Int's
         globalNumDeclNodesList = new ArrayList<NumDeclNode>();
         numDeclNode = new NumDeclNode("int1", new ExprNode(new NumberNode(1), new NumberNode(1), ExprOpEnum.ADD));
-        globalNumDeclNodesList.add(numDeclNode);
-    
+        
         //Creating global variables: Parts.
         List<PartDclNode> partDclNodesList = new ArrayList<PartDclNode>();
         PartDclNode partDclNode = new PartDclNode("part1", new StmtList());
-        partDclNodesList.add(partDclNode);
-    
+        
+        
         //Creating playNode
         StmtList statementList = new StmtList();
-        statementList.add(new NoteNode('c', new NumberNode(initialCtx.octave)));
+        statementList.add(numDeclNode);
         playNode = new PlayNode(statementList);
-    
+        
         //Creating progNode
         progNode = new ProgNode(globalNumDeclNodesList, partDclNodesList, playNode);
-    
+        
         symbolTable = new SymbolTable(progNode);
     }
     
     @Test
-    void testVisitProgNode ()
+    void testVisitPlayNode ()
     {
-        List<FinalNote> finalNoteList = visitor.visit(progNode);
-        assertEquals(1, finalNoteList.size());
-        assertEquals(Instrument.HARP, finalNoteList.iterator().next().instrument);
-        assertEquals(ToneEnum.C, finalNoteList.iterator().next().tone);
-        assertEquals(new FractionPrecise(1, 8), finalNoteList.iterator().next().timing);
-        assertEquals(8, finalNoteList.iterator().next().octave);
-        
+        visitor.visit(progNode);
+        assertNull(symbolTable.retrieveSymbol("int1"));
     }
-//
-//    @Test
-//    void testVisit1AndNode ()
-//    {
-//    }
-//
-//    @Test
-//    void testVisitBpmDclNode ()
-//    {
-//    }
-//
-//    @Test
-//    void testVisitEveryNode ()
-//    {
-//    }
-//
-//    @Test
-//    void testVisitIdNode ()
-//    {
-//    }
-//
-//    @Test
-//    void testVisitInstruNode ()
-//    {
-//    }
-//
-//    @Test
-//    void testVisitNoteNode ()
-//    {
-//    }
-//
-//    @Test
-//    void testVisitNodeContext ()
-//    {
-//    }
-//
-//    @Test
-//    void testVisitOctaveChangeNode ()
-//    {
-//    }
-//
-//    @Test
-//    void testVisitRepeatNode ()
-//    {
-//    }
-//
-//    @Test
-//    void testVisitStmtList ()
-//    {
-//    }
-//
-//    @Test
-//    void testVisitTempoChangeNode ()
-//    {
-//    }
-//
-//    @Test
-//    void testVisitTransposeNode ()
-//    {
-//    }
-//
-//    @Test
-//    void testVisitScaleNode ()
-//    {
-//    }
-//
-//    @Test
-//    void testVisitNumDeclNode ()
-//    {
-//    }
+    
+    @Test
+    void testVisitAnd ()
+    {
+        NodeContext expectedNodeContext = new NodeContext(initialCtx);
+        NodeContext leftContext = new NodeContext(initialCtx);
+        AndNode andNode = new AndNode(new NoteNode('c', new NumberNode(3)),
+                new TempoChangeNode(new NumberNode(81), new NumberNode(51)));
+        visitor.visit(andNode, initialCtx);
+        visitor.visit(new NoteNode('c', new NumberNode(3)), leftContext);
+        
+        expectedNodeContext.timing = leftContext.timing;
+        assertEquals(expectedNodeContext, initialCtx);
+    }
+    //
+    //    @Test
+    //    void testVisit1AndNode ()
+    //    {
+    //    }
+    //
+    //    @Test
+    //    void testVisitBpmDclNode ()
+    //    {
+    //    }
+    //
+    //    @Test
+    //    void testVisitEveryNode ()
+    //    {
+    //    }
+    //
+    //    @Test
+    //    void testVisitIdNode ()
+    //    {
+    //    }
+    //
+    //    @Test
+    //    void testVisitInstruNode ()
+    //    {
+    //    }
+    //
+    //    @Test
+    //    void testVisitNoteNode ()
+    //    {
+    //    }
+    //
+    //    @Test
+    //    void testVisitNodeContext ()
+    //    {
+    //    }
+    //
+    //    @Test
+    //    void testVisitOctaveChangeNode ()
+    //    {
+    //    }
+    //
+    //    @Test
+    //    void testVisitRepeatNode ()
+    //    {
+    //    }
+    //
+    //    @Test
+    //    void testVisitStmtList ()
+    //    {
+    //    }
+    //
+    //    @Test
+    //    void testVisitTempoChangeNode ()
+    //    {
+    //    }
+    //
+    //    @Test
+    //    void testVisitTransposeNode ()
+    //    {
+    //    }
+    //
+    //    @Test
+    //    void testVisitScaleNode ()
+    //    {
+    //    }
+    //
+    //    @Test
+    //    void testVisitNumDeclNode ()
+    //    {
+    //    }
 }
